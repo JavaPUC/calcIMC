34cbd5b8a7126af2947f614b1b32c664d6a416d2
nanaluvsu <worksender053@gmail.com>
2025-08-19 20:46:08 -0300

    ebaaaa eu esqueci q o buffer era fudidinho com scanner

--- Pessoa.java
+++ Pessoa.java
@@ -4,14 +4,50 @@ import java.util.Scanner;
 public class Pessoa {
     public static void main(String[] args) {
         System.out.println("Calculo imc eba");
+        
+        boolean oiii = false;
+        boolean tchauuu = false;
+        float peso = 0, altura = 0, imc = 0;
+
+
         Scanner sc = new Scanner(System.in);
-        try {
-            System.out.println("\nInsira seu peso: ");
-            double peso = sc.nextDouble();
-            System.out.println("\nInsira sua altura: ");
-            double altura = sc.nextDouble();
-            double imc = peso / (altura * altura);
-            System.out.println("\n\nIMC: " + imc + ". Classif: ");
+        
+        oiii = false;
+        while (oiii == false) {
+            try {
+                System.out.println("\nInsira seu peso: ");
+                
+                peso = sc.nextFloat();
+                
+                oiii = true;
+            } catch (InputMismatchException e) {
+                System.out.println("vc e idiota n pode letra");
+                sc.nextLine();
+            }
+        }
+
+        tchauuu = false;
+        while (tchauuu == false) {
+            try {
+                oiii = false;
+                while (oiii == false) {
+                    try {
+                        System.out.println("\nInsira sua altura: ");
+                        altura = sc.nextFloat();
+                        oiii = true;
+                    } catch (InputMismatchException e) {
+                        sc.nextLine();
+                        System.out.println("vc e idiota n pode letra");
+                    }
+                }
+                
+        imc = peso / (altura * altura);
+        tchauuu = true;
+            } catch (ArithmeticException e) {
+                System.out.println("Altura zero n existe :c");
+            } 
+
+            System.out.println("\n\nIMC: " + String.format("%.2f", imc) + ". Classif: ");
             if (imc < 18.5) {
                 System.out.println("Magreza.");
             } else if (imc >= 18.5 && imc <= 24.9) {
@@ -23,11 +59,8 @@ public class Pessoa {
             } else {
                 System.out.println("Obesidade grave.");
             }
-            sc.close();
-
-        } catch (InputMismatchException e) {
-            System.out.println("vc e idiota n pode letra");
         }
 
+        sc.close();
     }
 }
